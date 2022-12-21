a40bcbdcab53502eabec50c356bf4416c6169313
Kadir Kaan Göç <kkg@Kadir-MacBook-Air.local>
2022-12-21 18:19:41 +0300

    added

---
+++ BodyMassIndexCalc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BodyMassIndexCalc {
+    public static void main(String[] args) {
+        double weight, size, bodyMassIndex;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Boyunuzu metre cinsinden giriniz : ");
+        size = input.nextDouble();
+        System.out.print("Kilonuzu kg cinsinden giriniz : ");
+        weight = input.nextDouble();
+
+        bodyMassIndex = weight / (size * size);
+        System.out.println("Vücüt Kitle İndeksiniz : " + bodyMassIndex);
+    }
+}
